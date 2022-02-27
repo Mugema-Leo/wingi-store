77872003397968fafa8c172cf7d46b68e77eecc7
Mugema-Leo <lmugema@andrew.cmu.edu>
2022-02-27 20:35:04 +0200

    wing-store-update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,11 +16,9 @@ export default class Home extends Component {
     return (
       <div style={mystyle} className="bg-muted bg-image bg-image p-5 text-center shadow-1-strong rounded text-black d-flex justify-content-center align-items-center">
         <div>
-            <h1 class="mb-3 h2">Jumbotron</h1>
+            <h1 class="mb-3 h2">Wingi Store</h1>
             <div class="col-xs-1" align="center">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus praesentium
-                labore accusamus sequi, voluptate debitis tenetur in deleniti possimus modi voluptatum
-                neque maiores dolorem unde? Aut dolorum quod excepturi fugit.
+                From Wingi Store, you can make your own shop, and meet with your customers around the World.
             </div>
            <div>
             <Link to="/create"><button className='btn bg-success pt-3 text-white m-2'>Create your Store</button></Link>
